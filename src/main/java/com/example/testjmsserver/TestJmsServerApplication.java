0852cb5e8257e4805d9fd25ed85132dd53975057
Madan <madan712@gmail.com>
2020-02-08 11:49:24 +0000

    JMS server example

---
+++ src/main/java/com/example/testjmsserver/TestJmsServerApplication.java
@@ -0,0 +1,53 @@
+package com.example.testjmsserver;
+
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.jms.Queue;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsMessagingTemplate;
+
+@SpringBootApplication
+@EnableJms
+public class TestJmsServerApplication implements CommandLineRunner {
+
+	@Autowired
+	private JmsMessagingTemplate jmsMessagingTemplate;
+
+	@Autowired
+	private Queue queue;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJmsServerApplication.class, args);
+	}
+
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("myQueue");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				LocalDateTime time = LocalDateTime.now();
+				System.out.println("Sending message from server at " + time);
+				TestJmsServerApplication.this.jmsMessagingTemplate.convertAndSend(TestJmsServerApplication.this.queue,
+						"Hello JMS at " + time);
+
+			}
+		}, 0, 2000);
+
+	}
+
+}
